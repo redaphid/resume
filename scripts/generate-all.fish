7c48e9b0c0fd06aecc3b34a54e7521253855faa9
redaphid <iam@aaronherres.com>
2022-04-03 18:21:29 -0700

    generate-all is synchronous. Delete old resume.json. Remove themes I don't like. Try to add nodemon

--- scripts/generate-all.fish
+++ scripts/generate-all.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 nvm use 16
+rm out/*
 for t in (cat ./themes.json | jq -r .[])
   echo "generating theme $t"
-  npx resume-cli export --theme $t "./out/$t.html"&
+  npx resume-cli export --theme $t "./out/$t.html"
 end
