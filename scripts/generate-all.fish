b84268ae945ce0fc9820b6c9f0374b55f03be92c
redaphid <iam@aaronherres/com>
2023-03-08 12:22:25 -0700

    trying my own flexbox stuff

--- scripts/generate-all.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-nvm use 16
-mkdir -p out
-rm out/*
-for t in (cat ./themes.json | jq -r .[])
-  echo "generating theme $t"
-  npx resume-cli export --theme $t "./out/$t.html"
-end
-npx resume-cli export --theme ace "./out/ace.html"
